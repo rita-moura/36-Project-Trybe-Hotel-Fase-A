d4fd260011ac80e1140c5d66c173584560e9eed0
Rita Moura <ritamoura520@gmail.com>
2023-08-10 15:25:37 -0300

    Chose: Configura context.

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -5,5 +5,17 @@ namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
-    public TrybeHotelContext() { }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 }
