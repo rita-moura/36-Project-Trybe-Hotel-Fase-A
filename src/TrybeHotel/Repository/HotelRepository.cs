4fcfa4b4a4e72177831a6c9fb982ba9bf15d7589
Rita Moura <ritamoura520@gmail.com>
2023-08-10 17:55:30 -0300

    Feat: Implementa o metodo post da rota hotel.

--- src/TrybeHotel/Repository/HotelRepository.cs
+++ src/TrybeHotel/Repository/HotelRepository.cs
@@ -33,7 +33,26 @@ namespace TrybeHotel.Repository
         // 5. Desenvolva o endpoint POST /hotel
         public HotelDto AddHotel(Hotel hotel)
         {
-            throw new NotImplementedException();
+            var city = _context.Cities.FirstOrDefault(city => city.CityId == hotel.CityId);
+
+            var newHotel = new Hotel
+            {
+                Name = hotel.Name,
+                Address = hotel.Address,
+                CityId = hotel.CityId
+            };
+
+            _context.Hotels.Add(newHotel);
+            _context.SaveChanges();
+
+            return new HotelDto
+            {
+                HotelId = newHotel.HotelId,
+                Name = newHotel.Name,
+                Address = newHotel.Address,
+                CityId = newHotel.CityId,
+                CityName = city?.Name?? ""
+            };
         }
     }
 }
